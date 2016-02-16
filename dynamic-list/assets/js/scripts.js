a17e87218873cdb1b8b3f32837d87d6f449f586a
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-15 21:15:40 -0500

    trying a million different things

--- dynamic-list/assets/js/scripts.js
+++ dynamic-list/assets/js/scripts.js
@@ -7,9 +7,24 @@ $(function () {
 
 
   $('.text-box').on('click', function () {
-    $('<input type="text" class="text-box"/>').appendTo('.form-lists');
+    // $('<input type="text" class="text-box"/>:last').appendTo('.form-lists');
+
+    // $('<input type="text" class="text-box"/>').appendTo('.form-lists');
+
+    // $('<input type="text" class="text-box"/>').last().appendTo('.form-lists');
+
     // $('.form-lists').append('<input type="text" class="text-box"/>');
+
     // $('<input type="text" class="text-box"/>').clone().appendTo('.form-lists');
+
+    // $('.form-lists').append('<input type="text" class="text-box"/>');
+
+    // var addNew = $('.form-lists').append('<input type="text" class="text-box"/>');
+    //   addNew.last();
+    
+    var $addNew = $('<input type="text" class="text-box"/>');
+      $('input:last').after($addNew);
+
   });
 
 
