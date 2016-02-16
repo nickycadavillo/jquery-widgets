2aaa926d4eb6f274fa77b6de87b90e8657c24436
Nicky Cadavillo <nicoleacadavillo@gmail.com>
2016-02-15 20:02:49 -0500

    trying appendTo and clone()

--- dynamic-list/assets/js/scripts.js
+++ dynamic-list/assets/js/scripts.js
@@ -6,8 +6,10 @@ $(function () {
 
 
 
-  $('#text-box').on('click', function () {
-    $('.form-lists').append('<input type="text"/>');
+  $('.text-box').on('click', function () {
+    $('<input type="text" class="text-box"/>').appendTo('.form-lists');
+    // $('.form-lists').append('<input type="text" class="text-box"/>');
+    // $('<input type="text" class="text-box"/>').clone().appendTo('.form-lists');
   });
 
 
